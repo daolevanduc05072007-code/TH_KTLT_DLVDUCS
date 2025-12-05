3ea679a290618cf2be6a3b084566c8c6f855e5de
daolevanduc05072007-code <daolevanduc05072007@gmail.com>
2025-12-06 03:27:14 +0700

    Update Bai17.c

--- Bai17.c
+++ Bai17.c
@@ -21,7 +21,7 @@ int main()
 {
     printf( "NHAP 3 SO X, Y, Z:   ");
     scanf( "%lf %lf %lf" , &x , &y , &z );
-    printf( "max( x,y,z ) = %lf" , max3(x,y,z) );
+    printf( "\nmax( x,y,z ) = %lf" , max3(x,y,z) );
     printf( "\nmax( x+y+z , xyz ) =  %lf " , max2(x+y+z,x*y*z));
     printf( "\nmin^2( x+y+z/2 , xyz ) + 1 = %lf" , min2(x+y+z/2 , x*y*z )*min2(x+y+z/2 , x*y*z ) + 1 );    
 
