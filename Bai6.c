8e4c4a8c177292ff4454dd74f24af8062a77b1af
Dlvducs <daolevanduc05072007@gmail.com>
2025-12-06 11:04:44 +0700

    Update Bai6.c

--- Bai6.c
+++ Bai6.c
@@ -20,6 +20,7 @@ long double tong_phan_so(long double a,int n ) {
 }
 int main(){
     double n;
+    int N;
     double a;
     printf("Nhập vào số thực a và số nguyên n:");
     scanf("%lf %lf",&a,&n);
@@ -28,13 +29,13 @@ int main(){
         printf(" n phải là số nguyên (không chấp nhận số thập phân)!\n");
         return 0; 
     }
-    n = (int) n;
-    long double ket_qua_cau_a=tinh(a,n);
+    N = (int) n;
+    long double ket_qua_cau_a=tinh(a,N);
     printf("Kết quả câu a :%.10Lf\n",ket_qua_cau_a);
     
     //Tính câu b.
     //kiểm tra điều kiện
-    for (int i=0;i<n;i++) {
+    for (int i=0;i<N;i++) {
         if (a+i==0) {
             printf("Mau so bang 0 tai a+%d\n",i);
             printf("Cau b khong thuc hien duoc.");
@@ -42,7 +43,7 @@ int main(){
         }
     } 
 
-    long double tong=tong_phan_so(a,n);
+    long double tong=tong_phan_so(a,N);
     printf("Kết quả câu b:%.10Lf",tong);
 
     return 0;
